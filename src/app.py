32073458f9e8c284a801d4c4030ac6267a080a55
Pratyay Bishnupuri <pbishnupuri@wustl.edu>
2021-12-02 15:40:48 -0600

    added update rule

--- src/app.py
+++ src/app.py
@@ -3,18 +3,28 @@ from person import Person
 from article import Article
 from helper import Helper
 from graph import Graph
+import numpy as np
 
 
 def main():
+  # mu, sigma = 0.5, (0.5/3) # mean and standard deviation
+  # values = np.random.normal(mu, sigma, 100)
+  # for i in range(len(values)):
+  #   if values[i] < 0:
+  #     values[i] = 0
+  #   if values[i] > 1:
+  #     values[i] = 1
+  # print(max(values))
+  # print(min(values))
+
+
   helper = Helper()
-  # print("Hello World!")
-  p1 = Person(.5, .02, .25)
+  p1 = Person(.2, .9, .2)
   # print(p1)
   # helper.create_data("hello")
 
-
   # Creating articles
-  articles = [Article(.5), Article(0), Article(-0.5)]
+  articles = [Article(.1), Article(.8), Article(.3), Article(.5)]
   graph = Graph(articles)
   # print(graph.all_edges())
   # print(graph.all_vertices())
@@ -28,9 +38,15 @@ def main():
         paths = graph.find_all_paths(article, other)
         all_paths.append(paths[0])
 
-  print(all_paths)
+  
   all_stances = p1.simulate_paths(all_paths)
-  print(all_stances)
+  # print(all_paths)
+  # print(all_stances)
+
+  for i in range(len(all_paths)):
+    print(all_paths[i])
+    print(all_stances[i])
+    print('==================')
   
 
 if __name__ == "__main__":
--- src/person.py
+++ src/person.py
@@ -1,16 +1,37 @@
 # from article import Article
+import math
+import numpy as np
 
 class Person(object):
-  def __init__(self, stance, trust, empathy):
+  def __init__(self, stance, empathy, inertia):
     self.stance = stance
-    self.trust = trust
+    # self.trust = trust
     self.empathy = empathy
+    self.inertia = inertia
 
-  def update_func(self, article_bias):
-    self.stance = self.stance + article_bias
+  # def update_func(self, article_bias):
+  #   # self.stance = self.stance + article_bias
+  #   trust = math.exp(-(((article_bias - self.stance)**2)/(self.empathy)))
+  #   self.stance = (((self.inertia * self.stance)+(self.stance * trust)) / (self.inertia * trust))
+
+  def update_func2(self, stance, article_bias):
+    # self.stance = self.stance + article_bias
+    trust = math.exp(-(((article_bias - self.stance)**2)/(self.empathy)))
+    sig = np.sign(stance - article_bias)
+    if trust >= .5:
+          stance = stance + (1-self.inertia)*((1-self.inertia)*trust)*(article_bias - stance)
+    else:
+        stance = stance - (1-self.inertia)*((1-self.inertia)*trust)*(article_bias - stance)
+
+    # stance = ((self.inertia * stance)+(stance * trust)) / (self.inertia + trust)
+    print(f'article bias: {article_bias}')
+    print(f'trust: {trust}')
+    print(f'stance: {stance}')
+    print('==================')
+    return stance
   
-  def get_range(self):
-    return [max(-1, self.stance - self.empathy), min(self.stance + self.empathy, 1)]
+  # def get_range(self):
+  #   return [max(-1, self.stance - self.empathy), min(self.stance + self.empathy, 1)]
 
   def simulate_paths(self, paths):
     starting_stance = self.stance
@@ -18,7 +39,7 @@ class Person(object):
     for path in paths:
       current_stance = starting_stance
       for article in path:
-        current_stance += article.get_bias()
+        current_stance =  self.update_func2(current_stance, article.get_bias())
       all_stances.append(current_stance)
     
     return all_stances
